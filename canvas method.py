23ece4378ca0bccc97768d2e06125a406afce6dc
ikin5050 <60651617+ikin5050@users.noreply.github.com>
2020-02-11 16:59:25 +0000

    Add files via upload

--- canvas method.py
+++ canvas method.py
@@ -1,25 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 11 11:40:04 2020
-
-@author: Paul
-"""
-
 import numpy as np
+import cv2
 from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
 import tkinter as tk
-from tkinter import  Label
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-xtmi = 510
-xtma = 515
-xs = 1024
-ytmi = 382
-ytma = 387
-ys = 768
-
-
 
 #Define the target, source and output arrays. Source has to be completely white otherwise it kills everything
 def initialize(x,y):
@@ -35,7 +19,6 @@ def initialize(x,y):
         for n in range(y):
             source[n][i] = 1
     return target, source, output
-target,source,output=initialize(1024,768)
 
 # creates trap between XTrapMin-XTrapMax and YTrapMin-YTrapMax on Array
 def trap(xtmi,xtma,xs,ytmi,ytma,ys,array):
@@ -45,7 +28,6 @@ def trap(xtmi,xtma,xs,ytmi,ytma,ys,array):
                 if ytmi < n < ytma:
                     array[n][i] = 255
     return
-trap(xtmi,xtma,xs,ytmi,ytma,ys,target)
 
 #Returns the amplitude of a complex number
 def Amplitude(x):
@@ -70,103 +52,105 @@ def GS(target,source):
         A = np.fft.ifft2(D)
     output = Phase(A)
     return output
-output = GS(target,source)
 
 #Make array into PIL Image
 def mkPIL(array):
     im = Image.fromarray(np.uint8(array))
     return im
 
-def up(mi,ma):
-    mi += 1
-    ma += 1
+def up():
+    global ytmi
+    global ytma
+    ytmi += 10
+    ytma += 10
+    return 
+
+def down():
+    global ytmi
+    global ytma
+    ytmi -= 10
+    ytma -= 10
     return
 
-def down(mi,ma):
-    mi -= 1
-    ma -= 1
+def right():
+    global xtmi
+    global xtma
+    xtmi += 10
+    xtma += 10
     return
 
-def right(mi,ma):
-    mi += 1
-    ma += 1
-    return
-
-def left(mi,ma):
-    mi -= 1
-    ma -= 1
+def left():
+    global xtmi
+    global xtma
+    xtmi -= 10
+    xtma -= 10
     return
   
-im = mkPIL(output)
-plt.imshow(im, cmap='gray')
+xtmi = 510
+xtma = 515
+xs = 1024
+ytmi = 382
+ytma = 387
+ys = 768
+
 
 root = tk.Tk()
-
 def main():
-    
     app = Lower(root)
     root.mainloop()
-
 
 class Lower:
     def __init__(self, master):
         self.master = master
-        self.frame = tk.Frame(self.master)
-        self.frame.pack()
+        self.frame = tk.Frame(self.master).pack()
+        self.canvas = tk.Canvas(self.master, highlightthickness=0, borderwidth=0)
+        self.canvas.pack()
         self.button1 = tk.Button(self.frame, text = 'Create window', width = 25, command = self.new_window)
         self.button1.pack()
         self.displayimg = tk.Button(self.frame, text = 'Display', width = 25, command = self.plot)
         self.displayimg.pack()
+        self.makewidg()
+    def makewidg(self):
+        fig = plt.figure(figsize=(20,15), frameon=False)
+        self.ax = fig.add_subplot(111)
+        self.ax.set_yticklabels([])                        
+        self.ax.set_xticklabels([])
+        self.canvas = FigureCanvasTkAgg(fig, master=self.master)
+        self.canvas.get_tk_widget().pack()
+        self.canvas.draw()
     def new_window(self):
-        self.newWindow = tk.Toplevel(root)
+        self.newWindow = tk.Toplevel()
         self.app = Display(self.newWindow)
     def plot(self): 
-        fig = plt.figure(figsize=(20,15), frameon=False)
-        ax = fig.add_subplot(111)
-        ax.set_yticklabels([])                        
-        ax.set_xticklabels([])
-        plt.imshow(output, cmap='gray')
-        canvas = FigureCanvasTkAgg(fig, master=self.master)
-        canvas.get_tk_widget().pack()
-        canvas.draw()
-    def Recalculate(self):
-        target,source,output = initialize(xs,ys)
+        global xtmi, xtma, xs, ytmi, ytma, ys, i
+        target,source,output=initialize(xs,ys)
         trap(xtmi,xtma,xs,ytmi,ytma,ys,target)
         output = GS(target,source)
-        fig = plt.figure(figsize=(20,15), frameon=False)
-        ax = fig.add_subplot(111)
-        ax.set_yticklabels([])                        
-        ax.set_xticklabels([])
-        plt.imshow(output, cmap='gray')
-        canvas = FigureCanvasTkAgg(fig, master=self.master)
-        canvas.get_tk_widget().pack()
-        canvas.draw()
-    def close(self): 
-        self.master.destroy()
+        self.ax.imshow(output, cmap='gray')
+        self.canvas.draw()
+        self.ax.clear()
+        
+    def kill(self): 
+        root.destroy()
     
-# https://stackoverflow.com/questions/15999661/image-in-tkinter-window-by-clicking-on-button
-
 class Display:
     
     def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
         self.frame.pack()
-
-        self.up = tk.Button(self.frame, text = 'Up', width = 25, command = up(ytmi,ytma))
+        self.up = tk.Button(self.frame, text = 'Up', width = 25, command = up)
         self.up.pack()
-        self.down = tk.Button(self.frame, text = 'Down', width = 25, command = down(ytmi,ytma))
+        self.down = tk.Button(self.frame, text = 'Down', width = 25, command = down)
         self.down.pack()
-        self.right = tk.Button(self.frame, text =  'Right', width = 25, command = right(xtmi,xtma))
+        self.right = tk.Button(self.frame, text =  'Right', width = 25, command = right)
         self.right.pack()
-        self.left = tk.Button(self.frame, text = 'Left', width = 25, command = left(xtmi,xtma))
+        self.left = tk.Button(self.frame, text = 'Left', width = 25, command = left)
         self.left.pack()
-        self.rec = tk.Button(self.frame, text = 'Recalculate', width = 25, command = Lower.Recalculate(self))
-        self.rec.pack()
         self.kill = tk.Button(self.frame, text = 'Kill', width = 25, command = self.kill)
         self.kill.pack()
         
     def kill(self): 
-        self.master.destroy()
+        root.destroy()
  
 main()
